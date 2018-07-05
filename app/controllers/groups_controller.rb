bf3a95307fb84c5e18714d39475feeca30cd2464
yoshikyu <yoshikiki1109@yahoo.co.jp>
2018-07-05 21:22:16 +0900

    Edit group_view

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,14 +1,23 @@
 class GroupsController < ApplicationController
-  def new
+	def index	
+	end	
+	
+  def new #グループの新規作成画面
+    @group = Group.new #Groupモデルの新しいインスタンス
+    @group.users << current_user #現在ログイン中のカレントユーザーを新規作成したグループに追加
   end
 
-  def create
+  def create #グループ作成の可否に合わせた処理
+  	@group = Group.new(group_params)
+  	if @group.save
+  		redirect_to root_path, notice: 'グループを作成しました' #成功した時
+  	else
+  		render :new #失敗した時→該当のビューのみ表示。もう一回new画面をだす。これを省略したらcreateのビューにいく？
+  	end
   end
 
-  def update
+  private
+  def group_params
+  	params.require(:group).permit(:name, { :user_ids =>[]})
   end
-
-  def edit 
-  end
-
 end
