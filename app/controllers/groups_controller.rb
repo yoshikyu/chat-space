8c47749c6800f25d2d05be0b8fce55e05ec32486
yoshikyu <yoshikiki1109@yahoo.co.jp>
2018-07-06 00:15:31 +0900

    Edit Group

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,7 +1,8 @@
 class GroupsController < ApplicationController
+	before_action :set_group, only: [:edit, :update]
 	def index	
 	end	
-	
+
   def new #グループの新規作成画面
     @group = Group.new #Groupモデルの新しいインスタンス
     @group.users << current_user #現在ログイン中のカレントユーザーを新規作成したグループに追加
@@ -15,9 +16,20 @@ class GroupsController < ApplicationController
   		render :new #失敗した時→該当のビューのみ表示。もう一回new画面をだす。これを省略したらcreateのビューにいく？
   	end
   end
+  def update
+  	if @group.update(group_params)
+  		redirect_to group_messeges_path(@group), notice: 'グループを編集しました'
+  	else
+  		render :edit
+  	end
+  end
 
   private
   def group_params
   	params.require(:group).permit(:name, { :user_ids =>[]})
+  end
+
+  def set_group
+  	@group = Group.find(params[:id])
   end
 end
