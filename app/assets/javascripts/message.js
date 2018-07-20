82bd1a887fa4d1be0c69988e6ec5bb68224467fb
yoshikyu <yoshikiki1109@yahoo.co.jp>
2018-07-20 21:29:46 +0900

    再度調整

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,6 +1,6 @@
 $(function(){
-  function buildHTML(message){
-  	var html =
+　　function buildHTML(message){
+　　  var html =
 　　　　　　`<div class="message clearfix">
             <div class="upper--message">
               <div class="upper--message__user-name">
