8eaaeed9f1b3b12cb1f3f276040b7f618c1731e9
yoshikyu <yoshikiki1109@yahoo.co.jp>
2018-07-20 21:21:20 +0900

    列調整

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,49 +1,48 @@
 $(function(){
-	function buildHTML(message){
-		var html =
-`<div class="message clearfix">
-  <div class="upper--message">
-    <div class="upper--message__user-name">
-      ${ message.user_name }
-    </div>
-    <div class="upper--message__date"> 
-      ${ message.created_at }
-    </div>
-  </div>  
-  <div class="message--content"> 
-    <p class="bottom--message">
-      ${ message.content }
-      <img src = '${ message.image.url}', class='lower-message__image'>
-    </p>
-  </div>
-<div>`
-      return html;
-	}
-	$('#new_message').on('submit', function(e){
-	e.preventDefault();
-	var formData = new FormData(this);
-	var url = $(this).attr('action')  
-	$.ajax({
-		url:url,
-		type: "POST",
-		data: formData,
-		dataType:'json',
-		processData: false,
-		contentType: false
-	})
-	.done(function(data){
-		var html = buildHTML(data);
-		$('.messages').append(html)
-		$('.form__message').val('')
-		$('.messages').animate({scrollTop: $('.messages')[0].scrollHeight},4500,'linear' )
-		$('.form__submit').removeAttr('disabled')
-		$(".lower-message__image").error(function(){
-			$(this).remove();
-});
-	})
-
-	.fail(function(){
-		alert('error');
-	})
+  function buildHTML(message){
+  	var html =
+　　　　　　`<div class="message clearfix">
+            <div class="upper--message">
+              <div class="upper--message__user-name">
+                ${ message.user_name }
+              </div>
+              <div class="upper--message__date"> 
+                ${ message.created_at }
+              </div>
+            </div>  
+            <div class="message--content"> 
+              <p class="bottom--message">
+                ${ message.content }
+                <img src = '${ message.image.url}', class='lower-message__image'>
+              </p>
+            </div>
+　　　　　　<div>`
+　　return html;
+	　}
+　　$('#new_message').on('submit', function(e){
+	　  e.preventDefault();
+	　  var formData = new FormData(this);
+	　  var url = $(this).attr('action')  
+	　  $.ajax({
+		　  url:url,
+		　  type: "POST",
+		　  data: formData,
+		　  dataType:'json',
+		　  processData: false,
+		　  contentType: false
+	　　 })
+　　.done(function(data){
+		  var html = buildHTML(data);
+		  $('.messages').append(html)
+		  $('.form__message').val('')
+		  $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight},4500,'linear' )
+		  $('.form__submit').removeAttr('disabled')
+		  $(".lower-message__image").error(function(){
+			  $(this).remove();
+      });
+	  })
+	  .fail(function(){
+		  alert('error');
+	  })
   })
 });
--- app/views/messages/create.json.jbuilder
+++ app/views/messages/create.json.jbuilder
@@ -3,4 +3,3 @@ json.id @message.id
 json.created_at  @message.created_at.strftime('%Y/%m/%d %H:%M')
 json.content  @message.content
 json.image  @message.image  
-
