08cb6a3d327a845d799b1016332737bd33c5817f
yoshikyu <yoshikiki1109@yahoo.co.jp>
2018-07-20 20:27:13 +0900

    Add Asynchronous function

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -9,12 +9,16 @@ class MessagesController < ApplicationController
 
   def create
   	@message = @group.messages.new(messages_params)
-  	if @message.save
-  	  redirect_to group_messages_path(@group), notice: 'メッセージが送信されました'
-  	else
-  	  @messages = @group.messages.includes(:user)
-  	  flash.now[:alert] = 'メッセージを入力してください。'
-  	  render :index
+  	@message.save
+  	#   redirect_to group_messages_path(@group), notice: 'メッセージが送信されました'
+  	
+  	#   @messages = @group.messages.includes(:user)
+  	#   flash.now[:alert] = 'メッセージを入力してください。'
+  	#   # render :index
+
+    respond_to do |format|
+      format.html{ redirect_to group_messages_path(params[:group_id]) }
+      format.json
   	end
   end
 
---
+++ app/views/messages/create.json.jbuilder
@@ -0,0 +1,6 @@
+json.user_name  @message.user.name
+json.id @message.id
+json.created_at  @message.created_at.strftime('%Y/%m/%d %H:%M')
+json.content  @message.content
+json.image  @message.image  
+
