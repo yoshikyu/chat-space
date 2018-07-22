ebcfa3a748af1bdae549feb23c5548d11333e59c
yoshikyu <yoshikiki1109@yahoo.co.jp>
2018-07-22 20:43:01 +0900

    インデント調整

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -8,8 +8,8 @@ class MessagesController < ApplicationController
   end
 
   def create
-  	@message = @group.messages.new(messages_params)
-  	if @message.save
+    @message = @group.messages.new(messages_params)
+    if @message.save
       respond_to do |format|
         format.html{ redirect_to group_messages_path(params[:group_id]), notice: 'メッセージが送信されました' }
         format.json
