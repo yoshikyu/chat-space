e30b5b286f06f8f46333b7ed73956ce0c2961368
yoshikyu <yoshikiki1109@yahoo.co.jp>
2018-07-26 19:55:19 +0900

    インクリメンタルサーチ実装

---
+++ app/assets/javascripts/user.js
@@ -0,0 +1,70 @@
+$(function(){
+  function appendUser(user){
+    var html =
+      `<div class="chat-group-user clearfix">
+        <p class="chat-group-user__name">
+          ${ user.name }
+        </p>
+        <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id=${ user.id } data-user-name=${ user.name }>
+          追加
+        </a>
+      </div>`
+    $("#user-search-result").append(html);
+  };
+
+  function appendNoUser(user){
+    var html =
+      `<div class="chat-group-user clearfix">
+        <p class="chat-group-user__name">
+          ${ user }
+        </p>
+      </div>`
+    $("#user-search-result").append(html);
+  }
+
+  function addUserToGroup(userId, userName){
+    var html =
+      `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
+        <input name='group[user_ids][]' type='hidden' 'value=${ userId }'>
+        <p class='chat-group-user__name'>
+          ${ userName }
+        </p>
+        <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+      </div>`
+    $('.chat-group-form__field--bottom').append(html);
+  }
+
+	$("#user-search-field").on("keyup", function(){
+		var input = $(this).val();
+    $.ajax({
+    	type: "GET",
+    	url:"/users",
+    	data: {keyword:input},
+    	dataType: "json"
+    }) 
+    .done(function(users){
+    	$("#user-search-result").empty();
+      if (users.length !==0){
+        users.forEach(function(user){
+          appendUser(user);
+        });
+      }
+      else{
+        appendNoUser("一致するユーザーはいません")
+      }
+    })
+    .fail(function(){
+      alert("ユーザー検索に失敗しました");
+    })
+  });
+
+  $(document).on('click', '.user-search-add', function(){
+    var userId = $(this).attr("data-user-id")
+    var userName = $(this).attr("data-user-name")
+    $($(this).parent()).remove();
+    addUserToGroup(userId, userName);
+  })
+  $(document).on('click', '.js-remove-btn', function(){
+    $($(this).parent()).remove();
+  })
+});
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,12 @@
 class UsersController < ApplicationController
+
+  def index 
+    @users = User.where("name LIKE(?)", "%#{params[:keyword]}%")
+        respond_to do |format|
+        format.html
+        format.json
+      end
+  end
   
   def edit
   end
@@ -16,6 +24,6 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :email)
   end
-end
 
+end
 
